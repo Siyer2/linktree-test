b9fdcae6589f8022ef24fdf758a229ddc01c5e69
Syam Iyer <iyersyam@gmail.com>
2021-01-28 13:02:48 +1100

    Set up the index.ts file

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+import express, { Request, Response } from 'express';
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello world!");
+});
+
+// start the Express server
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
